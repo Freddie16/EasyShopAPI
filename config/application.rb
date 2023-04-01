8e4e714b1795030423e825162f5a97785c345f2b
Fred Murigi <freddiemurigi@gmail.com>
2023-04-01 18:16:28 +0300

    eleventh commit

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "rails/test_unit/railtie"
 require 'active_model_serializers'
 
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -33,6 +35,7 @@ module Ecommerce
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.sqlite3_production_warning = false
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
